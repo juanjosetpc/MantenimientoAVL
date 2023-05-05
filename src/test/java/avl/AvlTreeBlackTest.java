21045226fb14dfb03dee752e4f02ef01a264fd01
Javiserranoc <javiserranoc@uma.es>
2023-05-05 11:58:41 +0200

    necesario pa pull

--- src/test/java/avl/AvlTreeBlackTest.java
+++ src/test/java/avl/AvlTreeBlackTest.java
@@ -3,11 +3,34 @@ package avl;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class AvlTreeBlackTest {
     @Test
-    @DisplayName("")
-    public void validSearchNode(){
-        Object searchedItem = 4;
+    @DisplayName("Con un árbol vacío se inserta un único nodo, entonces sería el primero")
+    public void validInsertSingleNode(){
+        AvlTree arbol = new AvlTree(null);
+        arbol.insert(4);
+        Object itemExpected = 4;
+
+        assertEquals(itemExpected,arbol.top.getItem());
+    }
+
+    @Test
+    @DisplayName("Con un árbol vacío se inserta un único nodo, entonces sería el primero")
+    public void validInsertThreeNodes(){
+        AvlTree arbol = new AvlTree(comparator);
+        arbol.insert(4);
+        arbol.insert(8);
+        arbol.insert(2);
+        Object itemExpectedMiddle = 4;
+        Object itemExpectedFirst = 8;
+        Object itemExpectedLast = 2;
+
+        assertEquals(itemExpectedFirst,arbol.top.getItem());
+        assertEquals(itemExpectedMiddle,arbol.top.getLeft().getItem());
+        assertEquals(itemExpectedLast,arbol.top.getLeft().getLeft().getItem());
     }
 
     @Test
