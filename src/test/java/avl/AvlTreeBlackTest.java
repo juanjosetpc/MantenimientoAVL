185ac54915571c4008c508a9c88310d54b34e068
NICO\pupi1 <nzambrana20@uma.es>
2023-05-05 11:58:42 +0200

    black box testing

--- src/test/java/avl/AvlTreeBlackTest.java
+++ src/test/java/avl/AvlTreeBlackTest.java
@@ -3,7 +3,17 @@ package avl;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.Comparator;
+
+import static org.junit.Assert.*;
+
 public class AvlTreeBlackTest {
+    Comparator<Integer> comparator = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1.compareTo(o2);
+        }
+    };
     @Test
     @DisplayName("")
     public void validSearchNode(){
@@ -11,8 +21,73 @@ public class AvlTreeBlackTest {
     }
 
     @Test
-    @DisplayName("")
-    public void validDeleteNode(){
+    public void testInsertSingleNode() {
+        AvlTree<Integer> tree = new AvlTree<>(null);
+        tree.insert(5);
+        assertEquals(5, (int) tree.top.getItem());
+    }
 
+    @Test
+    public void testInsertMultipleNodes() {
+        AvlTree<Integer> tree = new AvlTree<>(comparator);
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(7);
+        assertEquals(5, (int) tree.top.getItem());
+        assertEquals(3, (int) tree.top.getLeft().getItem());
+        assertEquals(7, (int) tree.top.getRight().getItem());
+    }
+
+    @Test
+    public void testSearchExistingNode() {
+        AvlTree<Integer> tree = new AvlTree<>(null);
+        tree.insert(5);
+        AvlNode<Integer> node = tree.search(5);
+        assertNotNull(node);
+        assertEquals(5, (int) node.getItem());
+    }
+
+    @Test
+    public void testSearchNonExistingNode() {
+        AvlTree<Integer> tree = new AvlTree<>(null);
+        tree.insert(5);
+        AvlNode<Integer> node = tree.search(10);
+        assertNull(node);
+    }
+
+    @Test
+    public void testDeleteLeafNode() {
+        AvlTree<Integer> tree = new AvlTree<>(null);
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(7);
+        tree.delete(3);
+        assertEquals(5, (int) tree.top.getItem());
+        assertNull(tree.top.getLeft());
+        assertEquals(7, (int) tree.top.getRight().getItem());
+    }
+
+    @Test
+    public void testDeleteNodeWithALeftChild() {
+        AvlTree<Integer> tree = new AvlTree<>(null);
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(7);
+        tree.delete(5);
+        assertEquals(7, (int) tree.top.getItem());
+        assertEquals(3, (int) tree.top.getLeft().getItem());
+        assertNull(tree.top.getRight());
+    }
+
+    @Test
+    public void testDeleteNodeWithARightChild() {
+        AvlTree<Integer> tree = new AvlTree<>(null);
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(7);
+        tree.delete(7);
+        assertEquals(5, (int) tree.top.getItem());
+        assertEquals(3, (int) tree.top.getLeft().getItem());
+        assertNull(tree.top.getRight());
     }
 }
